24ade685e4b83f09c27dd9e88df1959d033f4545
Sandeep Kumar <sksandeepds37@gmail.com>
2026-04-21 13:51:33 +0530

    SignUp and signIn-user,admin,owner

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -66,7 +66,7 @@ export async function createUserController(request, response) {
 
     // generating json web token
     const token = jsonwebtoken.sign(
-      { user: user.id, email: user.email, role: user.role },
+      { user: user.id, email: user.email, userrole: user.role },
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRE || "1h" },
     );
@@ -78,6 +78,169 @@ export async function createUserController(request, response) {
 
     createTokenCookie(response, token, maxAge);
 
+    response.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating user", error);
+    response.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+}
+//create admin
+export async function createAdmin(request, response) {
+  // return if no user data
+  if (!request.body) {
+    return response.status(400).json({
+      success: false,
+      message: "Admin data required",
+    });
+  }
+
+  // destructing user details
+  const { name, email, password } = request.body;
+  const validateName = name ? nameValidator(name) : "";
+  const validateEmail = email ? emailValidator(email) : "";
+  const validatePassword = password ? passwordValidator(password) : "";
+  const role = "admin";
+
+  if (!validateName) {
+    return response.status(400).json({
+      success: false,
+      message: "Valid name is required",
+    });
+  }
+  if (!validateEmail) {
+    return response.status(400).json({
+      success: false,
+      message: "Valid email is required",
+    });
+  }
+  if (!validatePassword) {
+    return response.status(400).json({
+      success: false,
+      message:
+        "Password must contain combination of uppercase,lowercase,number,symbol and minimum 6 letters",
+    });
+  }
+
+  //encrypting password
+  const hashedPassword = await hashPassword(password);
+  const checkEmail = await User.checkEmailExist(email);
+  if (checkEmail) {
+    return response.status(400).json({
+      success: false,
+      message: "We have already a user with this email",
+    });
+  }
+
+  try {
+    const user = await User.createUser(name, email, hashedPassword, role);
+    if (!user) {
+      return response.status(400).json({
+        success: false,
+        message: "User creation failed",
+      });
+    }
+
+    // generating json web token
+    const token = jsonwebtoken.sign(
+      { user: user.id, email: user.email, userrole: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRE || "1h" },
+    );
+
+    //Storing cookie named token
+    const maxAge = process.env.JWT_EXPIRE
+      ? parseInt(process.env.JWT_EXPIRE) * 1000 * 60 * 60
+      : 3600000;
+
+    createTokenCookie(response, token, maxAge);
+
+    response.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating user", error);
+    response.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+}
+//create users
+export async function createUserOwner(request, response) {
+  const { userrole } = request.user;
+
+  if (userrole === "consumer" || userrole === "owner") {
+    return response.status(401).json({
+      success: false,
+      message: "Unauthorised access!",
+    });
+  }
+  // return if no user data
+  if (!request.body) {
+    return response.status(400).json({
+      success: false,
+      message: "User or Owner data required",
+    });
+  }
+
+  // destructing user details
+  const { name, email, password, role } = request.body;
+  const validateName = name ? nameValidator(name) : "";
+  const validateEmail = email ? emailValidator(email) : "";
+  const validatePassword = password ? passwordValidator(password) : "";
+
+  if (!validateName) {
+    return response.status(400).json({
+      success: false,
+      message: "Valid name is required",
+    });
+  }
+  if (!validateEmail) {
+    return response.status(400).json({
+      success: false,
+      message: "Valid email is required",
+    });
+  }
+  if (!validatePassword) {
+    return response.status(400).json({
+      success: false,
+      message:
+        "Password must contain combination of uppercase,lowercase,number,symbol and minimum 6 letters",
+    });
+  }
+  if (!role || (role !== "consumer" && role !== "owner")) {
+    return response.status(400).json({
+      message: "Role should be consumer or owner.",
+      success: false,
+    });
+  }
+  //encrypting password
+  const hashedPassword = await hashPassword(password);
+  const checkEmail = await User.checkEmailExist(email);
+  if (checkEmail) {
+    return response.status(400).json({
+      success: false,
+      message: "We have already a user with this email",
+    });
+  }
+
+  try {
+    const user = await User.createUser(name, email, hashedPassword, role);
+    if (!user) {
+      return response.status(400).json({
+        success: false,
+        message: "User creation failed",
+      });
+    }
     response.status(201).json({
       success: true,
       message: "User created successfully",
@@ -133,7 +296,7 @@ export async function loginController(request, response) {
     }
     // generating json web token
     const token = jsonwebtoken.sign(
-      { user: user.id, email: user.email, role: user.role },
+      { user: user.id, email: user.email, userrole: user.role },
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRE || "1h" },
     );
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,13 +1,17 @@
 import express from "express";
 import {
+  createAdmin,
   createUserController,
+  createUserOwner,
   loginController,
 } from "../controllers/usersController.js";
-
+import { verifyToken } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 //
 router.post("/", createUserController);
+router.post("/create/admin", createAdmin);
+router.post("/create", verifyToken, createUserOwner);
 router.post("/login", loginController);
 
 export default router;
